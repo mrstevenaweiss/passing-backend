f1fa2aed581fc5a21f20e2ca84c06f31315d3108
mrstevenaweiss <weiss.steven@gmail.com>
2018-08-17 15:04:20 -0400

    more backend stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, :posts
+      resources :users, only: [:index, :create, :destroy, :update]
+      resources :posts, only: [:index, :create, :destroy, :update]
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+steve = User.create(first_name: 'Steven', last_name: 'Weiss', email: 'weiss@weiss.com', password: '1234')
+larry = User.create(first_name: 'Lawrence', last_name: 'Hon', email: 'larry@larry.com', password: nil)
+
+met_on_l = Post.create(first_name: 'Steven', headline: 'Met in Union Square', email: 'weiss@weiss.com', where: 'Manhattan', encounter: 'I loved what you were wearing, etc. etc.', user_id: 1)
+
+met_on_school = Post.create(first_name: 'Larry', headline: 'Met at WeWork', email: 'larry@larry.com', where: 'dumbo', encounter: 'met at school', user_id: 3)
